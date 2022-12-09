b6821e420764b66b671f05a33a9848f32fdec138
weiserman <warrene@agile.co.za>
2022-12-09 17:52:30 +0200

    apply patches and unit tests

--- frappe_gym/frappe_gym/doctype/locker_booking/test_locker_booking.py
+++ frappe_gym/frappe_gym/doctype/locker_booking/test_locker_booking.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2022, Warren Eiserman and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestLockerBooking(FrappeTestCase):
-	pass
+	def test_cant_book_an_occupied_locker(self):
+		test_locker = frappe.get_doc({
+			"doctype" : "Locker",
+			"location" : "Test Location"
+		}).insert()
+
+		self.assertEqual(test_locker.available, True)
+
+		
+
+
